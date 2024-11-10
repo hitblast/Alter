ef36cf1f1703de05c465c24c6b55615ae96d7517
HitBlast <hitblastlive@gmail.com>
2024-11-11 00:01:39 +0600

    added basic window configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,36 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:desktop_window/desktop_window.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  Future<void> setWindowSize() async {
+    await DesktopWindow.setMinWindowSize(const Size(400, 400));
+    await DesktopWindow.setWindowSize(const Size(500, 500));
+    await DesktopWindow.setMaxWindowSize(const Size(600, 600));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setWindowSize();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return const CupertinoApp(
+      debugShowCheckedModeBanner: false,
+      home: CupertinoPageScaffold(
+        child: Center(child: Text('Drag app to change icon!')),
       ),
     );
   }
