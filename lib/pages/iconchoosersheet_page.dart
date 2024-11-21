a92cead826afeccbe992d6d16f55a08d2eff31af
HitBlast <hitblastlive@gmail.com>
2024-11-21 13:07:04 +0600

    adjusted constraints for window size

--- lib/pages/iconchoosersheet_page.dart
+++ lib/pages/iconchoosersheet_page.dart
@@ -32,85 +32,93 @@ class _IconChooserSheetPageState extends ConsumerState<IconChooserSheetPage> {
     return MacosSheet(
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'Choose icon for $appName',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 21,
-                color: CupertinoColors.systemGrey,
-                fontWeight: FontWeight.bold,
+        child: Center(
+          child: ScrollConfiguration(
+            behavior:
+                ScrollConfiguration.of(context).copyWith(scrollbars: false),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Choose icon for $appName',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 21,
+                      color: CupertinoColors.systemGrey,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Image(
+                        image: AssetImage('assets/alter_icon_frame.png'),
+                        width: 155,
+                        height: 155,
+                      ),
+                      GestureDetector(
+                        onTap: () async {
+                          final XFile? file = await pickIcon();
+
+                          if (file != null) {
+                            setState(() {
+                              currentPickedIcon = file;
+                            });
+                          }
+                        },
+                        child: Image(
+                          image: currentPickedIcon != null
+                              ? FileImage(File(currentPickedIcon!.path))
+                              : AssetImage('assets/alter_empty.png')
+                                  as ImageProvider,
+                          width: 145,
+                          height: 145,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 30),
+                  Text(
+                    currentPickedIcon != null
+                        ? 'You can modify by left-clicking again!'
+                        : 'Left-click above to choose a new icon.',
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      PushButton(
+                        controlSize: ControlSize.large,
+                        secondary: true,
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Cancel'),
+                      ),
+                      SizedBox(width: 10),
+                      Text('or'),
+                      SizedBox(width: 10),
+                      PushButton(
+                        controlSize: ControlSize.large,
+                        secondary: currentPickedIcon != null ? false : true,
+                        onPressed: () {
+                          if (currentPickedIcon == null) {
+                            return;
+                          }
+                          ref.read(appDatabaseNotifierProvider.notifier).addApp(
+                              widget.appFile.path, currentPickedIcon!.path);
+                          Navigator.of(context).pop();
+                        },
+                        child: Text('Apply Changes'),
+                      )
+                    ],
+                  ),
+                ],
               ),
             ),
-            SizedBox(height: 20),
-            Stack(
-              alignment: Alignment.center,
-              children: [
-                Image(
-                  image: AssetImage('assets/alter_icon_frame.png'),
-                  width: 155,
-                  height: 155,
-                ),
-                GestureDetector(
-                  onTap: () async {
-                    final XFile? file = await pickIcon();
-
-                    if (file != null) {
-                      setState(() {
-                        currentPickedIcon = file;
-                      });
-                    }
-                  },
-                  child: Image(
-                    image: currentPickedIcon != null
-                        ? FileImage(File(currentPickedIcon!.path))
-                        : AssetImage('assets/alter_empty.png') as ImageProvider,
-                    width: 145,
-                    height: 145,
-                  ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            Text(
-              currentPickedIcon != null
-                  ? 'You can modify by left-clicking again!'
-                  : 'Left-click above to choose a new icon.',
-            ),
-            SizedBox(height: 20),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                PushButton(
-                  controlSize: ControlSize.large,
-                  secondary: true,
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text('Cancel'),
-                ),
-                SizedBox(width: 10),
-                Text('or'),
-                SizedBox(width: 10),
-                PushButton(
-                  controlSize: ControlSize.large,
-                  secondary: currentPickedIcon != null ? false : true,
-                  onPressed: () {
-                    if (currentPickedIcon == null) {
-                      return;
-                    }
-                    ref
-                        .read(appDatabaseNotifierProvider.notifier)
-                        .addApp(widget.appFile.path, currentPickedIcon!.path);
-                    Navigator.of(context).pop();
-                  },
-                  child: Text('Apply Changes'),
-                )
-              ],
-            ),
-          ],
+          ),
         ),
       ),
     );
