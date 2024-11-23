10d3df3e63301c21058e80dab4f59341cfd2bf05
HitBlast <hitblastlive@gmail.com>
2024-11-23 14:40:52 +0600

    added basic toolbar and example buttons (to be improved later)

--- lib/pages/apps_page.dart
+++ lib/pages/apps_page.dart
@@ -1,6 +1,8 @@
 // Third-party imports.
+import 'package:alter/providers/app_database_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:macos_ui/macos_ui.dart';
 
 // The apps page.
 // This page is shown if the user has already populated Alter with apps to customize.
@@ -15,6 +17,41 @@ class AppsPage extends ConsumerStatefulWidget {
 class _AppsPageState extends ConsumerState<AppsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MacosScaffold(
+      toolBar: ToolBar(
+        title: const Text('Manage Apps'),
+        enableBlur: true,
+        actions: [
+          ToolBarIconButton(
+            icon: MacosIcon(
+              CupertinoIcons.add_circled,
+            ),
+            label: 'Add',
+            showLabel: true,
+            onPressed: () =>
+                debugPrint('Apps page: Toolbar add button clicked!'),
+          ),
+          ToolBarIconButton(
+            icon: MacosIcon(
+              CupertinoIcons.trash,
+            ),
+            label: 'Delete All',
+            showLabel: true,
+            onPressed: () {
+              ref.read(appDatabaseNotifierProvider.notifier).deleteAllApps();
+            },
+          ),
+          ToolBarIconButton(
+            icon: MacosIcon(
+              CupertinoIcons.question_circle,
+            ),
+            label: 'Help',
+            showLabel: true,
+            onPressed: () =>
+                debugPrint('Apps page: Toolbar help button clicked!'),
+          ),
+        ],
+      ),
+    );
   }
 }
