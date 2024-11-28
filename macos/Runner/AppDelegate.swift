be563af8a986faf50a1d45113ef37960a6e013d5
HitBlast <hitblastlive@gmail.com>
2024-11-28 23:21:48 +0600

    updated swift files

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -1,24 +1,30 @@
+// Imports.
 import Cocoa
 import FlutterMacOS
 
+// The AppDelegate class.
 @main
 class AppDelegate: FlutterAppDelegate {
-    override func applicationDidFinishLaunching(_ notification: Notification) {
-        NSApp.setActivationPolicy(.regular)
+    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        NSApp.setActivationPolicy(.accessory)
+        return false
     }
 
-    override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
-        NSApp.setActivationPolicy(.prohibited)
-        return false
+    override func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.regular)
     }
 
     override func applicationDidBecomeActive(_ notification: Notification) {
         NSApp.setActivationPolicy(.regular)
     }
 
-    override func applicationDidResignActive(_ notification: Notification) {
-        if NSApp.windows.filter({ $0.isVisible }).isEmpty {
-            NSApp.setActivationPolicy(.prohibited)
+    override func applicationShouldHandleReopen(
+        _ sender: NSApplication, hasVisibleWindows flag: Bool
+    ) -> Bool {
+        NSApp.setActivationPolicy(.regular)
+        if !flag, let window = NSApp.windows.first {
+            window.makeKeyAndOrderFront(self)
         }
+        return true
     }
 }
--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -3,15 +3,13 @@ import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
     override func awakeFromNib() {
-        let flutterViewController = FlutterViewController()
-        let windowFrame = self.frame
-        self.contentViewController = flutterViewController
-        self.setFrame(windowFrame, display: true)
+        super.awakeFromNib()
 
+        let flutterViewController = FlutterViewController()
+        self.contentViewController = flutterViewController
+        self.setFrame(self.frame, display: true)
         self.minSize = NSSize(width: 640, height: 430)
 
         RegisterGeneratedPlugins(registry: flutterViewController)
-
-        super.awakeFromNib()
     }
 }
