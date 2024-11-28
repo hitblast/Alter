8492ef7214ade7c2d8f988d548e50dceb5425ba1
HitBlast <hitblastlive@gmail.com>
2024-11-28 21:14:14 +0600

    add native code to keep the app running with icon / without

--- macos/Runner/AppDelegate.swift
+++ macos/Runner/AppDelegate.swift
@@ -3,7 +3,22 @@ import FlutterMacOS
 
 @main
 class AppDelegate: FlutterAppDelegate {
+    override func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.regular)
+    }
+
     override func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        NSApp.setActivationPolicy(.prohibited)
         return false
+    }
+
+    override func applicationDidBecomeActive(_ notification: Notification) {
+        NSApp.setActivationPolicy(.regular)
+    }
+
+    override func applicationDidResignActive(_ notification: Notification) {
+        if NSApp.windows.filter({ $0.isVisible }).isEmpty {
+            NSApp.setActivationPolicy(.prohibited)
+        }
     }
 }
