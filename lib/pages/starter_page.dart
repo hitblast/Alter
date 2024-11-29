586926d3d8a50bc1b77dd3aff1e18ac63b3cde59
HitBlast <hitblastlive@gmail.com>
2024-11-29 20:01:21 +0600

    added rive animation to the starter screen

--- lib/pages/starter_page.dart
+++ lib/pages/starter_page.dart
@@ -1,23 +1,16 @@
 // Third-party imports.
-import 'package:alter/utils/launch_url.dart';
+import 'package:rive/rive.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:macos_ui/macos_ui.dart';
 
 // Local imports.
 import 'package:alter/core/app_core.dart';
+import 'package:alter/utils/launch_url.dart';
 
 // The starter page widget.
 // This is the first page that the user sees if the have no apps added to the database yet.
-class StarterPage extends StatefulWidget {
+class StarterPage extends StatelessWidget {
   const StarterPage({super.key});
-
-  @override
-  State<StarterPage> createState() => _StarterPageState();
-}
-
-// The state of the starter page.
-class _StarterPageState extends State<StarterPage> {
-  var opacity = 0.5;
 
   @override
   Widget build(BuildContext context) {
@@ -56,30 +49,20 @@ class _StarterPageState extends State<StarterPage> {
                   padding: const EdgeInsets.all(20),
                   child: MouseRegion(
                     cursor: SystemMouseCursors.click,
-                    onEnter: (_) {
-                      setState(() {
-                        opacity = 0.8;
-                      });
-                    },
-                    onExit: (_) {
-                      setState(() {
-                        opacity = 0.5;
-                      });
-                    },
-                    child: AnimatedOpacity(
-                      opacity: opacity,
-                      curve: Curves.linearToEaseOut,
-                      duration: Duration(milliseconds: 250),
-                      child: const Column(
+                    child: Opacity(
+                      opacity: 0.78,
+                      child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Image(
-                            image: AssetImage(
-                                'assets/images/alter_empty_page_front.png'),
-                            width: 250,
-                            height: 250,
+                          SizedBox(
+                            width: 280,
+                            height: 280,
+                            child: RiveAnimation.asset(
+                              'assets/animations/alter_starter_screen.riv',
+                              fit: BoxFit.contain,
+                            ),
                           ),
-                          Text(
+                          const Text(
                             'Left-click to start customizing.',
                             style: TextStyle(
                               fontSize: 25,
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,6 +11,7 @@ import isar_flutter_libs
 import macos_ui
 import macos_window_utils
 import path_provider_foundation
+import rive_common
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -20,5 +21,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   MacOSUiPlugin.register(with: registry.registrar(forPlugin: "MacOSUiPlugin"))
   MacOSWindowUtilsPlugin.register(with: registry.registrar(forPlugin: "MacOSWindowUtilsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  RivePlugin.register(with: registry.registrar(forPlugin: "RivePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
