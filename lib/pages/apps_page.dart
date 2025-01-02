818256fd66a6dc4b3e46d9a73f461e975196c993
HitBlast <hitblastlive@gmail.com>
2025-01-02 13:33:50 +0600

    optimized apps page

--- lib/pages/apps_page.dart
+++ lib/pages/apps_page.dart
@@ -1,4 +1,3 @@
-// Third-party imports.
 import 'dart:io';
 
 import 'package:alter/utils/launch_url.dart';
@@ -6,13 +5,10 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:macos_ui/macos_ui.dart';
 
-// Local imports.
 import 'package:alter/core/app_core.dart';
 import 'package:alter/utils/file_picker.dart';
 import 'package:alter/providers/app_database_provider.dart';
 
-// The apps page.
-// This page is shown if the user has already populated Alter with apps to customize.
 class AppsPage extends ConsumerStatefulWidget {
   const AppsPage({super.key});
 
@@ -20,7 +16,6 @@ class AppsPage extends ConsumerStatefulWidget {
   ConsumerState<AppsPage> createState() => _AppsPageState();
 }
 
-// The state for the AppsPage widget.
 class _AppsPageState extends ConsumerState<AppsPage> {
   @override
   Widget build(BuildContext context) {
@@ -64,7 +59,6 @@ class _AppsPageState extends ConsumerState<AppsPage> {
             label: 'Kill Process',
             showLabel: true,
             onPressed: () {
-              // quit the app
               exit(0);
             },
           ),
@@ -92,15 +86,9 @@ class _AppsPageState extends ConsumerState<AppsPage> {
             bool isDarkMode =
                 MediaQuery.of(context).platformBrightness == Brightness.dark;
 
-            // Altering colors for the list items.
-            Color primaryColorDarkMode = Color.fromRGBO(40, 40, 40, 1.0);
             Color secondaryColorDarkMode = Color.fromRGBO(28, 28, 28, 1.0);
-            Color primaryColorLightMode = Color.fromRGBO(246, 246, 246, 1.000);
             Color secondaryColorLightMode =
                 Color.fromRGBO(228, 228, 228, 1.000);
-
-            var previousColor =
-                isDarkMode ? primaryColorDarkMode : primaryColorLightMode;
 
             return SingleChildScrollView(
               child: Column(
@@ -110,98 +98,93 @@ class _AppsPageState extends ConsumerState<AppsPage> {
                       builder: (context) {
                         var app = apps.value![i];
 
-                        // Alter the colors between the list items.
-                        if (isDarkMode) {
-                          previousColor = previousColor == primaryColorDarkMode
-                              ? secondaryColorDarkMode
-                              : primaryColorDarkMode;
-                        } else {
-                          previousColor = previousColor == primaryColorLightMode
-                              ? secondaryColorLightMode
-                              : primaryColorLightMode;
-                        }
-
-                        return MouseRegion(
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: previousColor,
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.only(
-                                top: 15,
-                                bottom: 15,
-                                left: 25,
-                                right: 25,
+                        return Column(
+                          children: [
+                            Container(
+                              decoration: BoxDecoration(
+                                color: isDarkMode
+                                    ? secondaryColorDarkMode
+                                    : secondaryColorLightMode,
                               ),
-                              child: Row(
-                                children: [
-                                  Text(
-                                    '${i + 1}.',
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold,
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                  top: 15,
+                                  bottom: 15,
+                                  left: 25,
+                                  right: 25,
+                                ),
+                                child: Row(
+                                  children: [
+                                    Text(
+                                      '${i + 1}.',
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
-                                  ),
-                                  const SizedBox(width: 20),
-                                  Container(
-                                    decoration: BoxDecoration(
-                                      boxShadow: [
-                                        BoxShadow(
-                                          color: CupertinoColors.black
-                                              .withOpacity(0.2),
-                                          blurRadius: 10,
-                                          spreadRadius: -3,
-                                          offset: Offset(0, 5),
+                                    const SizedBox(width: 20),
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        boxShadow: [
+                                          BoxShadow(
+                                            color: CupertinoColors.black
+                                                .withOpacity(0.2),
+                                            blurRadius: 10,
+                                            spreadRadius: -3,
+                                            offset: Offset(0, 5),
+                                          ),
+                                        ],
+                                      ),
+                                      child: MouseRegion(
+                                        cursor: SystemMouseCursors.click,
+                                        child: Image.file(
+                                          File(app.customIconPath),
+                                          width: 70,
+                                          height: 70,
                                         ),
-                                      ],
+                                      ),
                                     ),
-                                    child: Image.file(
-                                      File(app.customIconPath),
-                                      width: 70,
-                                      height: 70,
+                                    const SizedBox(width: 20),
+                                    Text(
+                                      getAppNameFromPath(app.path),
+                                      style: TextStyle(
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold,
+                                      ),
                                     ),
-                                  ),
-                                  const SizedBox(width: 20),
-                                  Text(
-                                    getAppNameFromPath(app.path),
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.bold,
+                                    const Spacer(),
+                                    PushButton(
+                                      onPressed: () {},
+                                      controlSize: ControlSize.regular,
+                                      secondary: true,
+                                      child: Text('Update Icon'),
                                     ),
-                                  ),
-                                  const Spacer(),
-                                  PushButton(
-                                    onPressed: () {},
-                                    controlSize: ControlSize.regular,
-                                    secondary: true,
-                                    child: Text('Update Icon'),
-                                  ),
-                                  const SizedBox(width: 10),
-                                  PushButton(
-                                    onPressed: () {
-                                      ref
-                                          .read(appDatabaseNotifierProvider
-                                              .notifier)
-                                          .deleteApp(app.id);
-                                    },
-                                    controlSize: ControlSize.regular,
-                                    secondary: true,
-                                    child: Text('Remove'),
-                                  ),
-                                ],
+                                    const SizedBox(width: 10),
+                                    PushButton(
+                                      onPressed: () {
+                                        ref
+                                            .read(appDatabaseNotifierProvider
+                                                .notifier)
+                                            .deleteApp(app.id);
+                                      },
+                                      controlSize: ControlSize.regular,
+                                      secondary: true,
+                                      child: Text('Remove'),
+                                    ),
+                                  ],
+                                ),
                               ),
                             ),
-                          ),
+                            Container(
+                              width: double.infinity,
+                              height: 1,
+                              color:
+                                  CupertinoColors.systemGrey.withOpacity(0.5),
+                            )
+                          ],
                         );
                       },
                     ),
-                  Container(
-                    width: double.infinity,
-                    height: 1.5,
-                    color: isDarkMode
-                        ? secondaryColorDarkMode
-                        : secondaryColorLightMode,
-                  )
                 ],
               ),
             );
