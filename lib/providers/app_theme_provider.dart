2690c885e34c66f3ee2baa83b2001c98301bceb4
HitBlast <hitblastlive@gmail.com>
2025-03-06 14:30:18 +0600

    updated app theme provider

--- lib/providers/app_theme_provider.dart
+++ lib/providers/app_theme_provider.dart
@@ -1,5 +1,6 @@
 // First-party imports.
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // Third-party imports.
 import 'package:riverpod_annotation/riverpod_annotation.dart';
@@ -9,6 +10,6 @@ part 'app_theme_provider.g.dart';
 
 /// The provider for ensuring dynamic theme change of the application.
 @riverpod
-ThemeMode themeMode(ThemeModeRef ref) {
+ThemeMode themeMode(Ref ref) {
   return ThemeMode.system;
 }
