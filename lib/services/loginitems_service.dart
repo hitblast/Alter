7a91effe5dbbf00e57f5de7a466c8ff60839acdf
HitBlast <hitblastlive@gmail.com>
2025-03-30 12:15:41 +0600

    added service for wrapping around MethodChannel calls

---
+++ lib/services/loginitems_service.dart
@@ -0,0 +1,38 @@
+// First-party imports.
+import 'package:flutter/services.dart';
+
+/// The LoginItemsService class provides a MethodChannel-based wrapper
+/// for managing login item entry for the project on a Mac.
+class LoginItemsService {
+  static const MethodChannel _channel = MethodChannel('alter_methods');
+
+  /// Registers the login item entry.
+  static Future<bool> enableLaunchAtLogin() async {
+    try {
+      final bool result = await _channel.invokeMethod('enableLaunchAtLogin');
+      return result;
+    } on PlatformException catch (e) {
+      throw 'Failed to enable launch at login: ${e.message}';
+    }
+  }
+
+  /// Unregisters the login item entry.
+  static Future<bool> disableLaunchAtLogin() async {
+    try {
+      final bool result = await _channel.invokeMethod('disableLaunchAtLogin');
+      return result;
+    } on PlatformException catch (e) {
+      throw 'Failed to disable launch at login: ${e.message}';
+    }
+  }
+
+  /// Checks if the login item entry is registered.
+  static Future<bool> isLaunchAtLoginEnabled() async {
+    try {
+      final bool result = await _channel.invokeMethod('isLaunchAtLoginEnabled');
+      return result;
+    } on PlatformException catch (e) {
+      throw 'Failed to retrieve launch at login status: ${e.message}';
+    }
+  }
+}
