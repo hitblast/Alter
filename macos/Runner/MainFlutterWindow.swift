a28b4da519d898bce4ca6420bdafd77077f8f6d3
HitBlast <hitblastlive@gmail.com>
2025-03-30 12:07:07 +0600

    added necessary MethodChannel integration to support LoginItems

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -1,5 +1,6 @@
 import Cocoa
 import FlutterMacOS
+import ServiceManagement
 import macos_window_utils
 
 class MainFlutterWindow: NSWindow {
@@ -8,6 +9,39 @@ class MainFlutterWindow: NSWindow {
         let windowFrame = self.frame
         self.contentViewController = flutterViewController
         self.setFrame(windowFrame, display: true)
+
+        FlutterMethodChannel(
+            name: "alter_methods", binaryMessenger: flutterViewController.engine.binaryMessenger
+        )
+        .setMethodCallHandler { (_ call: FlutterMethodCall, result: @escaping FlutterResult) in
+            switch call.method {
+            case "enableLaunchAtLogin":
+                do {
+                    try SMAppService.mainApp.register()
+                    result(true)
+                } catch {
+                    result(
+                        FlutterError(
+                            code: "ERROR_REGISTER", message: error.localizedDescription,
+                            details: nil))
+                }
+            case "disableLaunchAtLogin":
+                do {
+                    try SMAppService.mainApp.unregister()
+                    result(true)
+                } catch {
+                    result(
+                        FlutterError(
+                            code: "ERROR_UNREGISTER", message: error.localizedDescription,
+                            details: nil))
+                }
+            case "isLaunchAtLoginEnabled":
+                let isEnabled = SMAppService.mainApp.status == .enabled
+                result(isEnabled)
+            default:
+                result(FlutterMethodNotImplemented)
+            }
+        }
 
         RegisterGeneratedPlugins(registry: flutterViewController)
 
